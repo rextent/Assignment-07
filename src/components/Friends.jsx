ff09a15cd3229869a916584bfab0be4c7d140221
rextent <masudrana.fit@gmail.com>
2026-04-17 19:07:31 +0600

    Loading Spinner Added on Homepage

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -3,29 +3,46 @@ import FriendCard from "./FriendCard";
 
 const Friends = () => {
   const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("/friends.json")
       .then((res) => res.json())
-      .then((data) => setFriends(data));
+      .then((data) => {
+      setFriends(data)
+      setLoading(false)});
   }, []);
 
   return (
     <div className="container mx-auto px-4 py-10 lg:py-16">
-      
+
       {/* Heading */}
       <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800 mb-6">
         Your Friends
       </h2>
 
       {/* Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        
+
+      {loading ? (
+        <div className="flex justify-center items-center py-20">
+          <span className="loading loading-spinner text-info"></span>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {friends.map(friend => (
+            <FriendCard key={friend.id} friend={friend} />
+          ))}
+        </div>
+      )}
+
+
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
         {friends.map((friend) => (
           <FriendCard key={friend.id} friend={friend} />
         ))}
 
-      </div>
+      </div> */}
 
     </div>
   );
